fef2f2afae1fde8e023db7f83900f2eaa46e180c
gonggu <howareyou_mm@163.com>
2025-09-23 22:36:04 +0800

    完成Device相关功能的开发

--- src/context.cpp
+++ src/context.cpp
@@ -1,4 +1,5 @@
 #include "../toy2d/context.hpp"
+#include <iostream>
 namespace toy2d
 {
     std::unique_ptr<Context> Context::m_instance = nullptr;
@@ -12,13 +13,25 @@ namespace toy2d
     }
     Context& Context::GetInstance()
     {
+        assert(m_instance);
         return *m_instance;
     }
     Context::~Context()
     {
+        //this->_device.destroy();
         this->_instance.destroy();
+        
     }
     Context::Context()
+    {
+        CreateInstance();
+        PickupPhyiscalDevice();
+        QueryQueueFamilyIndices();
+        CreateDevice();
+        GetQueues();
+    }
+    
+    void Context::CreateInstance()
     {
         vk::InstanceCreateInfo insCreateInfo;
         vk::ApplicationInfo appInfo;
@@ -28,5 +41,42 @@ namespace toy2d
         std::vector<const char*> layers = { "VK_LAYER_KHRONOS_validation" };
         insCreateInfo.setPEnabledLayerNames(layers);
         this->_instance = vk::createInstance(insCreateInfo);
+    }
+
+    void Context::PickupPhyiscalDevice()
+    {
+        auto devices = _instance.enumeratePhysicalDevices();
+        this->_physicalDevice = devices[0];
+        std::cout << this->_physicalDevice.getProperties().deviceName << std::endl;
+    }
+    void Context::CreateDevice()
+    {
+        vk::DeviceCreateInfo createInfo;
+        vk::DeviceQueueCreateInfo queueCreatInfo;
+        float priorities = 1.0;
+        queueCreatInfo.setPQueuePriorities(&priorities)
+            .setQueueCount(1)
+            .setQueueFamilyIndex(_queueFamilyIndices.graphicsQueue.value());
+        createInfo.setQueueCreateInfos(queueCreatInfo);
+        _device = _physicalDevice.createDevice(createInfo);
+    }
+
+    void Context::QueryQueueFamilyIndices()
+    {
+        auto properties = _physicalDevice.getQueueFamilyProperties();
+        for (int i = 0; i < properties.size(); i++)
+        {
+            const auto& property = properties[i];
+            if (property.queueFlags | vk::QueueFlagBits::eGraphics)
+            {
+                _queueFamilyIndices.graphicsQueue = i;
+                break;
+            }
+        }
+    }
+
+    void Context::GetQueues()
+    {
+        _graphicsQueue = _device.getQueue(_queueFamilyIndices.graphicsQueue.value(), 0);
     }
 }
--- toy2d/context.hpp
+++ toy2d/context.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include <memory>
+#include <cassert>
+#include <optional>
+#include <iostream>
 #include "vulkan/vulkan.hpp"
 namespace toy2d
 {
@@ -12,11 +15,25 @@ namespace toy2d
         Context(const Context&) = delete;
         Context &operator=(const Context &) = delete;
         ~Context();
+    public:
+        struct QueueFamliyIndices final
+        {
+            std::optional<uint32_t> graphicsQueue;
+        };
+
+        QueueFamliyIndices _queueFamilyIndices;
     private:
         Context();
-
+        void CreateInstance();
+        void PickupPhyiscalDevice();
+        void CreateDevice();
+        void QueryQueueFamilyIndices();
+        void GetQueues();
     private:
         static std::unique_ptr<Context> m_instance;
         vk::Instance _instance;
+        vk::PhysicalDevice _physicalDevice;
+        vk::Device _device;
+        vk::Queue _graphicsQueue;
     };
 }
